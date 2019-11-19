a6efe3c03aad3d02b58c5333e83785864c70cdf4
Paul Catalan <contact.pcatl@gmail.com>
2019-11-19 20:12:42 +0800

    Added initial function for available actions

---
+++ actions.py
@@ -0,0 +1,10 @@
+
+def do_action(command):
+
+    if command == "test command":
+        print("test command 1")
+    else:
+        print("command not found")
+        return False
+
+    return True
